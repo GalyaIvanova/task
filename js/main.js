f05c407194fe348050f0aa643cf9104306dc765b
GalyaIvanova <32123443+GalyaIvanova@users.noreply.github.com>
2021-08-03 17:11:48 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 (function() {
     this.initializationTime = new Date();
     this.isRateGoingUp = true;
-    
+
     const shutDownTime = 300000;
-    const minRate= 1.0001;
+    const minRate = 1.0001;
 
     const currencyRatesRetriever = new CurrencyRatesRetriever();
     currencyRatesRetriever.get()
@@ -13,14 +13,14 @@
             // TODO: show some error
         });
 
-    this.initializeState = function (currencyData) {
+    this.initializeState = function(currencyData) {
         this.currencyData = currencyData;
-        this.updateRatesTimer = setInterval(this.updateRates, 5000);    // 5 sec (5000 ms).
-        this.ratesDirectionTimer = setInterval(this.changeRatesDirection, 60000);     // 60 sec (60000 ms).
+        this.updateRatesTimer = setInterval(this.updateRates, 5000); // 5 sec (5000 ms).
+        this.ratesDirectionTimer = setInterval(this.changeRatesDirection, 60000); // 60 sec (60000 ms).
         CreateTable(this.currencyData.rates);
     }
 
-    this.updateRates = function () {
+    this.updateRates = function() {
         const executionTime = new Date();
         if (executionTime - this.initializationTime >= shutDownTime) {
             this.clearTimers();
@@ -31,39 +31,34 @@
         keys.forEach(rateKey => {
             const rateValue = this.currencyData.rates[rateKey];
             let newRateValue = rateValue;
-            let spanRateValue =document.getElementById(rateKey);
-            if (this.isRateGoingUp){
-              
+            let spanRateValue = document.getElementById(rateKey);
+            if (this.isRateGoingUp) {
 
-                if(newRateValue>minRate){
+                if (newRateValue > minRate) {
                     newRateValue += 0.0001;
-                    spanRateValue.innerHTML=newRateValue.toFixed(4);
-    
-                }else{
+                    spanRateValue.innerHTML = newRateValue.toFixed(4);
+
+                } else {
                     newRateValue += 0.0001;
-                    spanRateValue.innerHTML=minRate;
-                   
+                    spanRateValue.innerHTML = minRate;
                 }
-                
-            }
-            else {
-                if(newRateValue>minRate){
+
+            } else {
+                if (newRateValue > minRate) {
                     newRateValue -= 0.0001;
-                    spanRateValue.innerHTML=newRateValue.toFixed(4);
-                }else{
+                    spanRateValue.innerHTML = newRateValue.toFixed(4);
+                } else {
                     newRateValue -= 0.0001;
-                    spanRateValue.innerHTML=minRate;
-                    
+                    spanRateValue.innerHTML = minRate;
                 }
-                
             }
 
-            
-            if(rateValue<newRateValue){
-                spanRateValue.setAttribute("class","up");
 
-            }else if(rateValue>newRateValue){
-                spanRateValue.setAttribute("class","down");
+            if (rateValue < newRateValue) {
+                spanRateValue.setAttribute("class", "up");
+
+            } else if (rateValue > newRateValue) {
+                spanRateValue.setAttribute("class", "down");
 
             }
             this.currencyData.rates[rateKey] = newRateValue;
@@ -75,37 +70,28 @@
         this.isRateGoingUp = !this.isRateGoingUp;
     }
 
-    this.clearTimers = function () {
+    this.clearTimers = function() {
         clearInterval(this.updateRatesTimer);
         clearInterval(this.ratesDirectionTimer);
     }
-    
-    function CreateTable(currencies){
+
+    function CreateTable(currencies) {
         let grid = document.querySelector('.parent');
-
-        
-
 
         const keys = Object.keys(currencies);
         keys.forEach(rateKey => {
 
-       
             let div = document.createElement('div');
             let span = document.createElement('span');
-            div.textContent ="EUR" + rateKey ;
+            div.textContent = "EUR" + rateKey;
             span.textContent = currencies[rateKey].toFixed(4);
             span.setAttribute("id", rateKey);
-            let row=div + span;
-          
-            
+
             grid.append(div);
             grid.append(span);
-
         });
-
-      
     }
-     
+  
     window.addEventListener('unload', function(event) {
         this.clearTimers();
     });
